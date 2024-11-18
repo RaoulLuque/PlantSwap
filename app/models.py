d1bdf3529e2b398e9017ce883799bd9fb6ead256
RaoulLuque <raoulsluque@gmail.com>
2024-11-18 14:09:04 +0100

    Add database model for trade requests. See https://github.com/fastapi/sqlmodel/issues/10#issuecomment-2153743134 for details of implementation. Towards #28
    Signed-off-by: RaoulLuque <raoulsluque@gmail.com>

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ import uuid
 from datetime import datetime
 
 from pydantic import BaseModel, EmailStr
+from sqlalchemy.orm import RelationshipProperty
 from sqlmodel import SQLModel, Field, Relationship
 
 
@@ -28,6 +29,20 @@ class User(UserBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     hashed_password: str
     plants: list["Plant"] = Relationship(back_populates="owner", cascade_delete=True)
+    incoming_requests: list["TradeRequest"] = Relationship(
+        sa_relationship=RelationshipProperty(
+            "TradeRequest",
+            back_populates="incoming_user",
+            foreign_keys="[TradeRequest.incoming_user_id]",
+        ),
+    )
+    outgoing_requests: list["TradeRequest"] = Relationship(
+        sa_relationship=RelationshipProperty(
+            "TradeRequest",
+            back_populates="outgoing_user",
+            foreign_keys="[TradeRequest.outgoing_user_id]",
+        )
+    )
 
 
 # Shared properties of plant ad in database
@@ -60,9 +75,35 @@ class PlantPublic(PlantBase):
     # description: str | None
 
 
+# Class to return multiple PlantPublic at the same time
 class PlantsPublic(SQLModel):
     data: list[PlantPublic]
     count: int
+
+
+# Link model (table) for many-to-many relationship of incoming and outgoing trade requests
+class TradeRequest(SQLModel, table=True):
+    outgoing_user_id: uuid.UUID = Field(
+        default_factory=uuid.uuid4, foreign_key="user.id", primary_key=True
+    )
+    outgoing_user: User = Relationship(
+        sa_relationship=RelationshipProperty(
+            "User",
+            back_populates="outgoing_requests",
+            foreign_keys="[TradeRequest.outgoing_user_id]",
+        )
+    )
+    incoming_user_id: uuid.UUID = Field(
+        default_factory=uuid.uuid4, foreign_key="user.id", primary_key=True
+    )
+    incoming_user: User = Relationship(
+        sa_relationship=RelationshipProperty(
+            "User",
+            back_populates="incoming_requests",
+            foreign_keys="[TradeRequest.incoming_user_id]",
+        )
+    )
+    message: str | None = Field(default=None, max_length=255)
 
 
 # Token data class for JWT Encoding
