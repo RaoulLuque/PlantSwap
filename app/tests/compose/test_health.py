6cace48ada34a84f5a26c69ef89078eb4af0583b
RaoulLuque <raoulsluque@gmail.com>
2024-12-29 18:14:58 +0100

    Add docker-compose.override (and other necessary) file(s) for testing if application is reachable when started using docker compose.
    Signed-off-by: RaoulLuque <raoulsluque@gmail.com>

---
+++ app/tests/compose/test_health.py
@@ -0,0 +1,10 @@
+import pytest
+from httpx import AsyncClient
+
+
+@pytest.mark.asyncio
+async def test_health_endpoint():
+    async with AsyncClient(base_url="http://plantswap-app:8000") as client:
+        response = await client.get("/")
+        assert response.status_code == 200
+        assert response.json() == {"message": "Hello World!"}
