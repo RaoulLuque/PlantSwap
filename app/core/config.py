82245aab992a6b95281e452b3c4e2d7fab814cb6
RaoulLuque <raoulsluque@gmail.com>
2025-01-15 12:48:52 +0100

    Change config to use .env file only if it exists and environment variables on the machine if no .env file exists
    Signed-off-by: RaoulLuque <raoulsluque@gmail.com>

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 import secrets
 from pydantic import PostgresDsn, computed_field
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 from pydantic_core import MultiHostUrl
 
 
@@ -9,25 +9,19 @@ class Settings(BaseSettings):
     Settings class containing .env file settings
     """
 
-    model_config = SettingsConfigDict(
-        # Use top level .env file (one level above ./app/)
-        env_file="../.env",
-        env_ignore_empty=True,
-        extra="ignore",
-    )
     # SECRETS
     SECRET_KEY: str = secrets.token_urlsafe(32)
-    FIRST_SUPERUSER: str = "admin@example.com"
-    FIRST_SUPERUSER_PASSWORD: str = "password"
+    FIRST_SUPERUSER: str
+    FIRST_SUPERUSER_PASSWORD: str
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
 
     # POSTGRES
-    POSTGRES_SERVER: str = "localhost"
+    POSTGRES_SERVER: str
     POSTGRES_PORT: int = 5432
-    POSTGRES_USER: str = "postgres"
-    POSTGRES_PASSWORD: str = "password"
-    POSTGRES_DB: str = "postgres"
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
 
     @computed_field  # type: ignore[prop-decorator]
     @property
@@ -40,6 +34,11 @@ class Settings(BaseSettings):
             port=self.POSTGRES_PORT,
             path=self.POSTGRES_DB,
         )
+
+    class Config:
+        # Automatically load from a `.env` file for local development (if .env file exists, otherwise use environment variables)
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
 
 settings = Settings()  # type: ignore
