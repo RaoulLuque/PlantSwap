cc5d0389755bc0188c180149fefdf42afe8021a9
RaoulLuque <raoulsluque@gmail.com>
2025-01-28 22:10:04 +0100

    Add top bar with user, logo and future option to create plant ad
    Signed-off-by: RaoulLuque <raoulsluque@gmail.com>

--- app-frontend/src/App.js
+++ app-frontend/src/App.js
@@ -1,22 +1,40 @@
-import {Routes, Route, BrowserRouter} from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import PlantsRouter from './routers/plants/plants_router';
 import NotFoundPage from "./routers/not_found/not_found_router";
-import {ChakraProvider} from "@chakra-ui/react";
+import {Box, ChakraProvider} from "@chakra-ui/react";
+import WithAction from "./pages/top_bar.js"; // Import the WithAction component
+
+// Layout component that includes the top bar
+const Layout = ({ children }) => {
+  return (
+    <>
+      <WithAction /> {/* Top bar */}
+      <Box p={4}>{children}</Box> {/* Page content */}
+    </>
+  );
+};
 
 function App() {
   return (
-      <ChakraProvider>
-        <BrowserRouter>
-            <div className="App">
-              <Routes>
-                {/* Include the PlantsRouter component for plants-related routes */}
-                <Route path="/plants/*" element={<PlantsRouter />} />
+    <ChakraProvider>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            {/* Wrap the PlantsRouter with the Layout component */}
+            <Route
+              path="/plants/*"
+              element={
+                <Layout>
+                  <PlantsRouter />
+                </Layout>
+              }
+            />
 
-                {/* Add the catch-all route for 404 Not Found */}
-                <Route path="*" element={<NotFoundPage />} />
-              </Routes>
-            </div>
-        </BrowserRouter>
+            {/* Render NotFoundPage without the Layout */}
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
