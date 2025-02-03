1f6cdaf3a1b8db4df4e864d91ef5543685f38e70
RaoulLuque <raoulsluque@gmail.com>
2025-02-03 12:05:30 +0100

    Add BACKEND URL as environment variable for Vercel deployment
    Signed-off-by: RaoulLuque <raoulsluque@gmail.com>

--- app-frontend/src/api.js
+++ app-frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Create an axios instance with the base URL of the backend API
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: process.env.REACT_APP_BACKEND_URL,
 })
 
 export default api;
