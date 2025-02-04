ac028ea1983052e2ba4c4f61caab7ecf61f4ebaa
RaoulLuque <raoulsluque@gmail.com>
2025-02-05 00:02:07 +0100

    Add further logging to Backend
    Signed-off-by: RaoulLuque <raoulsluque@gmail.com>

--- app/main.py
+++ app/main.py
@@ -13,6 +13,17 @@ from .core.images import set_cloudinary_config
 # Initialize the FastAPI app
 app = FastAPI(title="PlantSwap")
 
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f"{exc}".replace("\n", " ").replace("   ", " ")
+    logging.error(f"{request}: {exc_str}")
+    content = {"status_code": 10422, "message": exc_str, "data": None}
+    return JSONResponse(
+        content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+    )
+
+
 # Set up logger
 logger = logging.getLogger("uvicorn.error")
 logger.info(msg="----- ----- APPLICATION STARTING ----- -----")
