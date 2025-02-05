18857ce49eb8872d4e1094b56d9958be1e61a040
RaoulLuque <raoulsluque@gmail.com>
2025-02-05 12:21:01 +0100

    Rename plants_list to plants
    Signed-off-by: RaoulLuque <raoulsluque@gmail.com>

--- app-frontend/src/routers/main/main_router.js
+++ app-frontend/src/routers/main/main_router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import PlantList from "./pages/plants_list";
+import PlantList from "./pages/plants";
 
 const MainRouter = () => {
   return (
